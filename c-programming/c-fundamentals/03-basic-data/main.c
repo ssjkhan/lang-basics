7aaac64ae1bb42cf529dc966d5d0132e85c40229
sk <jsckhan@outlook.com>
2023-04-09 04:52:30 -0400

    c-programming/ basic data types

---
+++ c-programming/c-fundamentals/03-basic-data/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// function prototypes
+void printSizes (void );
+  
+
+int main () {
+// basic data
+printSizes();
+    return 0;
+}
+
+void printSizes(void){
+  printf("Size of C data types \n\n");
+  printf("Type \t Bytes\n\n");
+
+}
